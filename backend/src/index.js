1e521842fe2e9f28ead068484e80791efde6740d
ebertmota <ebert.mota03@gmail.com>
2020-01-16 21:13:02 -0300

    building web interface

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 const dotenv = require('dotenv').config();
 
 const app = express();
 mongoose.set('useCreateIndex', true);
-mongoose.connect(process.env.connectionString,
-  {
+mongoose.connect(process.env.connectionString,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+app.use(cors({ origin: 'http://localhost:3000' }))
 app.use(express.json());
 app.use(routes);
 
